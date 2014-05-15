858e71d683fbd85b13b39076a6ad804123d2a1d6
andrewe <andrewevansc@gmail.com>
2014-05-15 14:53:04 -0700

    Can CRUD artists
    Former-commit-id: 38e01e1d06002ba34b93f1af66df813b7d44df04

--- app/models/Artist.php
+++ app/models/Artist.php
@@ -12,17 +12,33 @@ class Artist extends Eloquent
      */
     protected $table = 'artists';
 
-    protected $fillable = ['slug','first_name'];
+    protected $fillable = ['slug', 'alias', 'first_name', 'last_name', 'url_slug', 'meta_title', 'meta_description', 'year_begin', 'year_end'];
 
 
     public static $rules = array(
+        'first_name'       => 'required',
+        'last_name'       => 'required',
+        'alias'       => 'required',
         'slug'       => 'required',
-        'first_name'      => 'required',
+        'url_slug'       => 'required',
+        'year_begin'      => 'required|numeric|min:0',
+        'year_end'      => 'required|numeric|min:0',
     );
 
     public static $messages = [
-        'slug.required' => "You need a cost. No free lunches.",
+        'slug.required' => "You need a slug.",
     ];
 
+    public function isValid($id = null)
+    {
+        $rules_modified = static::$rules;
+
+        $validation = Validator::make($this->attributes, $rules_modified,  static::$messages);
+
+        if ($validation->passes()) return true;
+
+        $this->errors = $validation->messages();
+        return false;
+    }
 
 }
---
+++ app/views/artists/edit.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.default')
+<!-- app/views/artists/edit.blade.php -->
+
+@section('content')
+
+<h1>Edit {{ $artist->alias }}</h1>
+
+<!-- if there are creation errors, they will show here -->
+{{ HTML::ul($errors->all()) }}
+
+{{ Form::model($artist, array('route' => array('artists.update', $artist->id), 'method' => 'PUT')) }}
+
+<div class="form-group">
+    {{ Form::label('first_name', 'First Name') }}
+    {{ Form::text('first_name', null, array('class' => 'form-control')) }}
+</div>
+
+<div class="form-group">
+    {{ Form::label('last_name', 'Last Name') }}
+    {{ Form::text('last_name', null, array('class' => 'form-control')) }}
+</div>
+
+<div class="form-group">
+    {{ Form::label('alias', 'Alias') }}
+    {{ Form::text('alias', null, array('class' => 'form-control')) }}
+</div>
+
+<div class="form-group">
+    {{ Form::label('slug', 'Slug') }}
+    {{ Form::text('slug', null, array('class' => 'form-control')) }}
+</div>
+
+<div class="form-group">
+    {{ Form::label('url_slug', 'URL Slug') }}
+    {{ Form::text('url_slug', null, array('class' => 'form-control')) }}
+</div>
+
+<div class="form-group">
+    {{ Form::label('meta_title', 'meta Title') }}
+    {{ Form::text('meta_title', null, array('class' => 'form-control')) }}
+</div>
+
+<div class="form-group">
+    {{ Form::label('meta_description', 'meta Description') }}
+    {{ Form::text('meta_description', null, array('class' => 'form-control')) }}
+</div>
+
+<div class="form-group">
+    {{ Form::label('year_begin', 'Year Begin') }}
+    {{ Form::text('year_begin', null, array('class' => 'form-control')) }}
+</div>
+
+<div class="form-group">
+    {{ Form::label('year_end', 'Year End') }}
+    {{ Form::text('year_end', null, array('class' => 'form-control')) }}
+</div>
+
+
+{{ Form::submit('Edit the Artist!', array('class' => 'btn btn-primary')) }}
+
+{{ Form::close() }}
+
+@stop
