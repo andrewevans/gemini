cd155840400f9d95c4f921a3cbcf3735aeec1abe
andrewe <andrewevansc@gmail.com>
2014-05-19 18:09:44 -0700

    placeholder image used if artist profile image not available
    Former-commit-id: f7935877d2642f1b0cdd620ea1dc7b256e7989ac

--- app/views/artists/show.blade.php
+++ app/views/artists/show.blade.php
@@ -12,6 +12,8 @@
         <strong>url_slug:</strong> {{ $artist->url_slug }}<br />
         @if (file_exists($artist->img_url))
             <strong>Avatar:</strong> {{ HTML::image($artist->img_url, 'Profile of ' . $artist->alias) }}<br />
+        @else
+            <strong>Avatar:</strong> {{ HTML::image('img/no-image.jpg', 'Profile of ' . $artist->alias) }}<br />
         @endif
 
     </p>
