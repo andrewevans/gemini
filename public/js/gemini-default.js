4c84d775333306fdf1fdac43fcb49b769f80f9a8
andrewe <andrewevansc@gmail.com>
2014-05-31 21:55:32 -0700

    design improvements all over, especially on artist's page

--- public/js/gemini-default.js
+++ public/js/gemini-default.js
@@ -10,7 +10,8 @@ $( window ).resize(function() {
 
 function resize_carousel()
 {
-    dog = $( window ).height() - parseInt($('body').css('padding-top')) - 58;
-    $('.carousel').css('height', dog);
-    $('.carousel .item').css('height',dog);
+    carousel_height = $( window ).height() - parseInt($('body').css('padding-top')) - 58;
+    if (carousel_height > 800) carousel_height = 800;
+    $('.carousel').css('height', carousel_height);
+    $('.carousel .item').css('height',carousel_height);
 }
