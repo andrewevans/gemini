815b7191d6011cf0a48afb0dd7139e799ab26768
andrewe <andrewevansc@gmail.com>
2014-06-02 15:50:03 -0700

    design stuff
    Former-commit-id: bf2eae093c04a8ecbbaecf344023a70375e0a56c

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -14,9 +14,39 @@
         <li data-target="#myCarousel" data-slide-to="4"></li>
     </ol>
     <div class="carousel-inner">
-        <?php $count = 0; ?>
+        <div class="item active">
+            <div class="color-text" style="cursor: pointer;">
+                <div class="ttl" style="position: relative; top: 0px; opacity: 1;">
+                    <h1>Pablo Picasso</h1>
+                    <h2>It is your work in life that is the ultimate seduction.</h2>
+                    <p>Picasso revolutionized the art world and to many is THE artist of the 20th century. He is famous for his role in pioneering Cubism with Georges Braque and for his melancholy Blue Period pieces.</p>
+                </div>
+                <a href="#">
+                    <span>
+                        View All
+                    </span>
+                </a>
+                <a href="#">
+                    <span>
+                         | Ceramics
+                    </span>
+                </a>
+                <a href="#">
+                    <span>
+                         | Linocuts
+                    </span>
+                </a>
+            </div>
+
+            <img src="http://www.masterworksfineart.com/images/splashes/picasso-jacqueline-for-sale.jpg">                                <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <?php $count = 1; ?>
         @foreach ($artworks as $key => $artwork)
-        <div class="item <?= ($key == 0 ? 'active' : '') ?>">
+        <div class="item <?= ($key == -1 ? 'active' : '') ?>">
             @if (file_exists('img/artists/' . $artwork->artist->slug . '/original/' . $artwork->artist->slug . $artwork->id . '.jpg'))
             {{ HTML::image('img/artists/' . $artwork->artist->slug . '/original/' . $artwork->artist->slug . $artwork->id . '.jpg') }}
             @else
