f5967268d5bb4f8ed172495aaeffc9ffab567a9f
andrewe <andrewevansc@gmail.com>
2014-06-12 21:21:55 -0700

    search typeahead sends id (called guid) from api through if the user picks the option from the suggestions in the drop-down list. Also, some styling.
    Former-commit-id: 6af2eb94fe7063f74e647d4a0b85bb655bcb257d

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -39,7 +39,7 @@
 <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
         <div class="logo">
-            <!--<a href="http://www.masterworksfineart.com"><img src="http://www.masterworksfineart.com/foxy/public/skins/blues/images/masterworks-fine-art.gif" alt="Masterworks Fine Art Gallery"></a>-->
+            <a href="http://www.masterworksfineart.com"><img src="http://www.masterworksfineart.com/foxy/public/skins/blues/images/masterworks-fine-art.gif" alt="Masterworks Fine Art Gallery"></a>
         </div>
         <div class="contact-info">
             510-777-9970 / <span id="phone_number_800">800-805-7060</span><br>
@@ -52,7 +52,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ URL::to('/') }}">Gemini</a>
+            <!-- <a class="navbar-brand" href="{{ URL::to('/') }}">Gemini</a>-->
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
@@ -70,26 +70,30 @@
                         @endforeach
                     </ul>
                 </li>
-                <li><a href="{{ URL::to('artworks') }}">Artworks</a></li>
-                <li><a href="#">Art Education</a></li>
-                <li><a href="{{ URL::to('blog') }}">Blog</a></li>
-                <li><a href="#">Buying From Us</a></li>
-                <li><a href="#">About Us</a></li>
+                <li><a href="wp.appelfineart.com">Blog</a></li>
+                <li><a href="#">Education</a></li>
+                <li><a href="#">Why Choose Us</a></li>
                 <li><a href="#">Contact Us</a></li>
                 <li><a href="#">Sell Your Fine Art</a></li>
-                <li>
-                    <div id="multiple-datasets">
-                        {{ Form::open(array('method' => 'get', 'url' => 'search')) }}
-                            <div class="form-group">
-                                {{ Form::label('q', 'Search') }}
-                                {{ Form::text('q', null, array('class' => 'typeahead form-control', 'placeholder' => 'Search artists or artworks')) }}
-                            </div>
-                            {{ Form::submit('Search', array('class' => 'btn btn-primary')) }}
-                        {{ Form::close() }}
+            </ul>
+            <div id="multiple-datasets" class="col-sm-3 col-md-3 pull-right">
+                {{ Form::open(array('method' => 'get', 'url' => 'search', 'class' => 'navbar-form navbar-right', 'role' => 'search')) }}
+                    <div class="input-group">
+                        {{ Form::label('q', 'Search') }}
+                        {{ Form::text('q', null, array('class' => 'typeahead form-control', 'placeholder' => 'Search artists or artworks', 'autocomplete' => 'off')) }}
+                        {{ Form::hidden('q_id', null, array('class' => 'q_id', 'id' => 'q_id')) }}
+
+                        <div class="input-group-btn">
+                            <button class="btn btn-default">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </div>
+
                     </div>
 
-                </li>
-            </ul>
+                {{ Form::close() }}
+            </div>
+
         </div><!--/.nav-collapse -->
     </div>
 </div>
@@ -117,87 +121,18 @@
 {{ HTML::script('js/jquery-2.1.1.min.js') }}
 {{ HTML::script('vendor/bootstrap/docs.min.js') }}
 {{ HTML::script('vendor/bootstrap/js/bootstrap.min.js') }}
-{{ HTML::script('js/gemini-default.js') }}
 <!--<link href="/vendor/jquery-ui/css/ui-lightness/jquery-ui-1.10.4.css" rel="stylesheet">-->
 <!--<script src="/vendor/jquery-ui/js/jquery-ui-1.10.4.js"></script>-->
 <script src="/vendor/typeahead.js-master/dist/bloodhound.js"></script>
 <script src="/vendor/typeahead.js-master/dist/typeahead.bundle.js"></script>
 <script src="/vendor/handlebars-v1.3.0.js"></script>
+{{ HTML::script('js/gemini-default.js') }}
 
 <script>
     CKEDITOR.replaceAll();
 </script>
 
 <script>
-    var charMap = {'àáâããäå': 'a', 'èéêë': 'e', 'ç': 'c', 'ß': 'ss'};
-
-    var normalize = function(str) {
-        $.each(charMap, function(chars, normalized) {
-            var regex = new RegExp('[' + chars + ']', 'gi');
-            str = str.replace(regex, normalized);
-        });
-
-        return str;
-    }
-
-    var queryTokenizer = function(q) {
-        var normalized = normalize(q);
-        return Bloodhound.tokenizers.whitespace(normalized);
-    };
-
-    var artists_list = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        prefetch: {
-            ttl: 0,
-            url: '/api/v1/url/artists',
-        }
-    });
-
-    var artworks_list = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-        queryTokenizer: queryTokenizer,
-        prefetch: {
-            ttl: 0,
-            url: '/api/v1/url/artworks'
-        }
-    });
-
-    artists_list.initialize();
-    artworks_list.initialize();
-
-    $('#multiple-datasets .typeahead').typeahead({
-            highlight: true
-        },
-        {
-            name: 'artists-list',
-            displayKey: 'value',
-            source: artists_list.ttAdapter(),
-            templates: {
-                header: '<h3 class="table-name">Artists</h3>',
-                empty: [
-                    '<div class="empty-message">',
-                    '<p>No artists found.</p>',
-                    '</div>'
-                ].join('\n'),
-                suggestion: Handlebars.compile('<p><strong>\{\{value\}\}</strong>  (\{\{year_begin\}\} - \{\{year_end\}\})</p>')
-            }
-
-        },
-        {
-            name: 'artworks-list',
-            displayKey: 'value',
-            source: artworks_list.ttAdapter(),
-            templates: {
-                header: '<h3 class="table-name">Artworks</h3>',
-                empty: [
-                    '<div class="empty-message">',
-                    '<p>No artwork found.</p>',
-                    '</div>'
-                ].join('\n'),
-                suggestion: Handlebars.compile('<p><strong>\{\{value\}\}</strong>  (\{\{medium_short\}\})</p>')
-            }
-        });
 </script>
 </body>
 </html>
