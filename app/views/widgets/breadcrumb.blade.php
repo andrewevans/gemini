59ffe10baba715493f620f4ab2ab4faf3589f49c
andrewe <andrewevansc@gmail.com>
2014-06-16 23:41:54 -0700

    major design upgrades, including for all screens
    Former-commit-id: 1e961b0a77c4485d3a1a0d3e7500e54d8d370a73

---
+++ app/views/widgets/breadcrumb.blade.php
@@ -0,0 +1,29 @@
+<ol class="breadcrumb">
+    <li><a href="/" title="Fine art original prints"><span class="glyphicon glyphicon-home"></span></a></a></li>
+
+    @if('artists.index' == Route::current()->getName() )
+    <li class="active">BUY ORIGINALS</li>
+    @endif
+
+    @if('artists.show' == Route::current()->getName() )
+    <li><a href="/artists">BUY ORIGINALS</a></li>
+    <li class="active">{{ $artwork->artist->alias }}</li>
+    @endif
+
+    @if('artists.show.filter' == Route::current()->getName() )
+    <li><a href="/artists">BUY ORIGINALS</a></li>
+    <li><a href="{{ $artwork->artist->url() }}">{{ $artwork->artist->alias }}</a></li>
+    <li class="active">{{ $filter }}</li>
+    @endif
+
+    @if('artworks.showOne' == Route::current()->getName() )
+    <li><a href="/artists">BUY ORIGINALS</a></li>
+    <li><a href="{{ $artwork->artist->url() }}">{{ $artwork->artist->alias }}</a></li>
+    <li class="active">{{ $artwork->mediums() }}</li>
+    @endif
+
+    @if('search.index' == Route::current()->getName() )
+    <li class="active">Fine art search results for <b>{{ $q }}</b></li>
+    @endif
+
+</ol>
