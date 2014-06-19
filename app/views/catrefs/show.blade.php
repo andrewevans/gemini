8688bd883bdadeaf8964138f8afa4a574a5122db
andrewe <andrewevansc@gmail.com>
2014-06-19 16:38:01 -0700

    sip - uses external images from live site to showcase Sorlier's catalogue with images. Database now contains all sorlier's catalogue data

--- app/views/catrefs/show.blade.php
+++ app/views/catrefs/show.blade.php
@@ -3,11 +3,46 @@
 
 @section('content')
 
-<div class="container">
-    <div class="intro-header" style="text-align: center; max-width:450px;"> <i class="sprite-h gtitle-deco above"></i>
-        <h1>{{ $page_title }}</h1>
-        <p>{{ $catref->reference_num }}</p>
-    </div>
-</div>
+<div class="jumbotron text-center">
+    <p>
+        <a href="{{ "http://www.masterworksfineart.com/catalogue/chagall/sorlier/original/sorlier" . $catref->reference_num . ".jpg" }}">
+            @if ($catref->catalogue->slug == "sorlier")
+            {{ HTML::image("http://www.masterworksfineart.com/catalogue/chagall/sorlier/original/sorlier" . $catref->reference_num . ".jpg", 'Image of ' . $catref->title) }}<br />
+            @else
+            {{ HTML::image('img/no-image.jpg', 'No image available') }}<br />
+            @endif
+        </a>
+    </p>
 
+    <table class="table text-left">
+        <tbody>
+        <tr>
+            <th>Artist</th><td> <b><a href="{{ $catref->catalogue->artist->url_slug }}">{{ $catref->catalogue->artist->last_name }}, {{ $catref->catalogue->artist->first_name }}</a></b></td>
+        </tr>
+        <tr>
+            <th>Catalogue</th><td> {{ $catref->catalogue->title }}</td>
+        </tr>
+        <tr>
+            <th>Reference #</th><td> {{ $catref->reference_num }}</td>
+        </tr>
+        <tr>
+            <th>Title</th><td> {{ $catref->title }}</td>
+        </tr>
+        <tr>
+            <th>Size</th><td> {{ $catref->size }}</td>
+        </tr>
+        <tr>
+            <th>Edition</th><td> {{ $catref->edition }}</td>
+        </tr>
+        <tr>
+            <th>Medium</th><td> {{ $catref->medium }}</td>
+        </tr>
+        <tr>
+            <th>Signature</th><td> {{ $catref->signed }}</td>
+        </tr>
+        </tbody>
+    </table>
+
+    </p>
+</div>
 @stop
