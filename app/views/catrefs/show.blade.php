9400eaa4da4899a533b13f37456432cac16ef46b
andrewe <andrewevansc@gmail.com>
2014-06-27 12:44:22 -0700

    Cleaned up catrefs
    Former-commit-id: 7bf8e5db7ae9370f008095b7ef70918848437ffb

--- app/views/catrefs/show.blade.php
+++ app/views/catrefs/show.blade.php
@@ -8,13 +8,7 @@
     <div class="row">
         <div class="col-md-4 col-md-offset-2">
             <figure>
-                @if ($catref->catalogue->slug == "sorlier")
-                {{ HTML::image('http://www.masterworksfineart.com/catalogue/' . $catref->catalogue->artist->slug . '/' . $catref->catalogue->slug . '/original/' . $catref->catalogue->slug . $catref->reference_num . '.jpg') }}<br />
-                @elseif (file_exists($catref->img_url()))
-                {{ HTML::image($catref->img_url()) }}<br />
-                @else
-                {{ HTML::image('img/no-image.jpg', 'Profile of ' . $catref->catalogue->artist->alias) }}<br />
-                @endif
+                {{ HTML::image($catref->img_url()) }}
                 <figcaption>
                     <i>{{ $catref->title }}</i>, {{ $catref->catalogue->artist->alias }}, from {{ $catref->catalogue->title }}
                 </figcaption>
@@ -26,13 +20,13 @@
             <table class="table text-left catref specs">
                 <tbody>
                 <tr>
-                    <th>Artist</th><td> <b><a href="{{ $catref->catalogue->artist->url_slug }}">{{ $catref->catalogue->artist->last_name }}, {{ $catref->catalogue->artist->first_name }}</a></b></td>
+                    <th>Artist</th><td> <b><a href="{{ $catref->catalogue->artist->url() }}">{{ $catref->catalogue->artist->last_name }}, {{ $catref->catalogue->artist->first_name }}</a></b></td>
                 </tr>
                 <tr>
                     <th>Title</th><td> {{ $catref->title }}</td>
                 </tr>
                 <tr>
-                    <th>Catalogue</th><td> {{ $catref->catalogue->title }}</td>
+                    <th>Catalogue</th><td> <a href="{{ $catref->catalogue->url() }}">{{ $catref->catalogue->title }}</a></td>
                 </tr>
                 <tr>
                     <th>Reference #</th><td> {{ $catref->reference_num }}</td>
