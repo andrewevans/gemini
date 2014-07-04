09daedf71282c65b2ce5c004ee26f75a11faf968
andrewe <andrewevansc@gmail.com>
2014-07-04 14:34:32 -0700

    breadcrumbs for static pages

--- app/controllers/StaticController.php
+++ app/controllers/StaticController.php
@@ -14,7 +14,27 @@ class StaticController extends \BaseController {
 
         if ($post == null) App::abort(404);
 
-        return View::make('widgets.page', ['post' => $post]);
+        $breadcrumb = $this->breadcrumb($post);
+
+        return View::make('widgets.page', ['breadcrumb' => $breadcrumb, 'post' => $post]);
+    }
+
+    public function breadcrumb($post)
+    {
+        $breadcrumb[] = '<li class="active">' . $post->post_title . '</li>';
+
+        while ($this->the_parent($post)) {
+            $post = $this->the_parent($post);
+            $breadcrumb[] = '<li><a href="' . str_replace(get_site_url() , '', get_permalink($post->ID)) . '">' . $post->post_title . '</a></li>';
+        }
+
+        return implode(' ', array_reverse($breadcrumb));
+    }
+
+    public function the_parent($post) {
+        if($post->post_parent == 0) return false;
+        $post_data = get_post($post->post_parent);
+        return $post_data;
     }
 
 }
