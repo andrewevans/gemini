d4879ea4a8dc2e82560a4f1abc581425c94910f7
andrewe <andrewevansc@gmail.com>
2014-07-04 20:48:50 -0700

    API homepage
    Former-commit-id: cbeb55c45b73c5e066c6992b6c602a1f21991683

---
+++ app/views/url/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+<!-- app/views/artworks/show.blade.php -->
+
+@section('content')
+
+<div class="container">
+    <h1>Welcome to API!</h1>
+
+    <ul>
+        <li>Artists</li>
+        <li>Artworks</li>
+        <li>Catalogues</li>
+        <li>Catrefs</li>
+        <li>Artists</li>
+    </ul>
+</div>
+@stop
