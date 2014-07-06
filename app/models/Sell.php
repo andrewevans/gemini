db318629fab5f7b20741cb715af0ccb198a2cd4b
andrewe <andrewevansc@gmail.com>
2014-07-05 17:04:16 -0700

    /sell , and improved contact email form
    Former-commit-id: f0ce704115377285a89d93cb7db65e452c66b5bc

---
+++ app/models/Sell.php
@@ -0,0 +1,40 @@
+<?php
+// app/models/Sell.php
+
+class Sell extends Eloquent
+{
+    public $timestamp = false;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'sells';
+
+    protected $fillable = ['cust_email', 'cust_name', 'cust_inquiry'];
+
+
+    public static $rules = array(
+        'cust_email'       => 'required|email',
+        'cust_inquiry'       => 'required',
+    );
+
+    public static $messages = [
+        'cust_email.required' => "You need an email address!",
+        'cust_email.email' => "Uho, that's not a valid email address.",
+        'cust_inquiry.required' => "You need an inquiry.",
+    ];
+
+    public function isValid($id = null)
+    {
+
+        $validation = Validator::make($this->attributes, static::$rules,  static::$messages);
+
+        if ($validation->passes()) return true;
+
+        $this->errors = $validation->messages();
+        return false;
+    }
+
+}
