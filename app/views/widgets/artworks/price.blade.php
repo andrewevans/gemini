708b5ae1caf9e30599aec8b42eac3f1556e3b123
andrewe <andrewevansc@gmail.com>
2014-07-11 15:04:48 -0700

    Improved mobile design.

--- app/views/widgets/artworks/price.blade.php
+++ app/views/widgets/artworks/price.blade.php
@@ -1,7 +1,10 @@
 <p>
     <span>USD ${{ number_format($artwork->price) }}</span>
 
-    <a href="/offer?artwork_id={{ $artwork->id }}" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-pencil"></span> Submit Best Offer</a>
-    <a href="/purchase?artwork_id={{ $artwork->id }}" class="btn btn-default" role="button"><span class="glyphicon glyphicon-star"></span> Purchase Now</a>
-    <a href="#" class="btn btn-default" role="button"> Questions?</a>
+    <span class="actions">
+        <a href="/offer?artwork_id={{ $artwork->id }}" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-pencil"></span> Submit Best Offer</a>
+        <a href="/purchase?artwork_id={{ $artwork->id }}" class="btn btn-default" role="button"><span class="glyphicon glyphicon-star"></span> Purchase Now</a>
+        <a href="#" class="btn btn-default" role="button"> Questions?</a>
+    </span>
+
 </p>
