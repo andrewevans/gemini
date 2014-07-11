d0cb02b5353161b60a0c2d1d269368cd16139c87
andrewe <andrewevansc@gmail.com>
2014-07-11 02:10:10 -0700

    Improved design
    Former-commit-id: 2e4e73e08b42e087a99eac636a3bb897aff76a90

--- app/views/widgets/page.blade.php
+++ app/views/widgets/page.blade.php
@@ -2,9 +2,17 @@
 <!-- app/views/artists/show.blade.php -->
 
 @section('content')
+<div class="container">
+    <h1>{{ $post->post_title }}</h1>
 
-<h1>{{ $post->post_title }}</h1>
+    <div class="row">
+        <div class="col-md-9">
+            {{ apply_filters('the_content',$post->post_content); }}
+        </div>
+        <div class="col-md-3">
 
-{{ apply_filters('the_content',$post->post_content); }}
+        </div>
+
+</div>
 
 @stop
