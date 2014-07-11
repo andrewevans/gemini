ed65def87b1308d160f5cf1fa3cd14434a0160bd
andrewe <andrewevansc@gmail.com>
2014-07-11 15:04:16 -0700

    List filters on catalogue catrefs
    Former-commit-id: 0d98eed891c3b8dd119d5372914b0b545e0d1eb6

--- app/views/widgets/artists/filters.blade.php
+++ app/views/widgets/artists/filters.blade.php
@@ -2,8 +2,7 @@
     @if (sizeof($artist->filters()) > 0)
         <div>Filter By:</div>
         @foreach ($artist->filters() as $filterBy)
-        <button type="button" class="btn btn-default <?= ($filter_slug == $filterBy ? 'active' : '') ?>"><a href=" {{ $artist->url() . '/' . $filterBy }}">{{ $filterBy }}</a></button>
+        <button type="button" class="btn btn-default <?= (isset($filter_slug) && $filter_slug == $filterBy ? 'active' : '') ?>"><a href=" {{ $artist->url() . '/' . $filterBy }}">{{ $filterBy }}</a></button>
         @endforeach
     @endif
 </div>
-
