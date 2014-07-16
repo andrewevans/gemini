2cc8c932f35c5bf8f2e38c37e9ee2722805a9c90
andrewe <andrewevansc@gmail.com>
2014-07-15 19:22:35 -0700

    Improved artists nag dropdown menu. Includes a new artists method inverted_alias
    Former-commit-id: 51ffbcbf51863a88ad99836ca9e33500940d205f

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -78,19 +78,80 @@
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Artists <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ URL::to('artists') }}">Artists</a></li>
-                        <li class="divider"></li>
-                        <li class="dropdown-header">Featured Artists</li>
-                            <li><a href="/artists/pablo-picasso">Pablo Picasso</a></li>
-                        <li class="divider"></li>
-                        <li class="dropdown-header">All Artists</li>
-                        @foreach ($artists as $artist_each)
-                        <li><a href="{{ $artist_each->url() }}">{{ $artist_each->alias }}</a></li>
-                        @endforeach
-                    </ul>
+                    <a href="/artists" class="dropdown-toggle active" data-toggle="dropdown">Artists <b class="caret"></b></a>
+                    <div class="dropdown-menu container" style="margin-left:-90px">
+                        <div class="col-md-3"><ul class="nav-multi-list featured">
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Featured Artists</li>
+                                <li class="divider"></li>
+                                <li><a href="/artists/georges-braque">Georges Braque</a></li>
+                                <li><a href="/artists/marc-chagall">Marc Chagall</a></li>
+                                <li><a href="/artists/albrecht-durer">Albrecht D&uuml;rer</a></li>
+                                <li><a href="/artists/rene-magritte">Ren&eacute; Magritte</a></li>
+                                <li><a href="/artists/henri-matisse">Henri Matisse</a></li>
+                                <li><a href="/artists/joan-miro">Joan Mir&oacute;</a></li>
+                                <li><a href="/artists/claude-monet">Claude Monet</a></li>
+                                <li><a href="/artists/pablo-picasso">Pablo Picasso</a></li>
+                                <li><a href="/artists/pablo-picasso/ceramics">Picasso Ceramics</a></li>
+                                <li><a href="/artists/rembrandt">Rembramdt</a></li>
+                                <li><a href="/artists/renoir">Renoir</a></li>
+                                <li><a href="/artists/andy-warhol">Andy Warhol</a></li>
+                                <li><a href="/artists/victor-vasarely">Victor Vasarely</a></li>
+                                <li><a href="/artists/yvaral">Yvaral</a></li>
+                                <li><a href="/artists?filter=featured" style="text-transform: uppercase">View All Artists &thinsp;»</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-3">
+                            <ul class="nav-multi-list">
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Modern Masters</li>
+                                <li class="divider"></li>
 
+                                @foreach ($artists as $artist_each)
+                                    @if ($artist_each->genre == "modern")
+                                        <li><a href="{{ $artist_each->url() }}">{{ $artist_each->inverted_alias() }}</a></li>
+                                    @endif
+                                @endforeach
+                            </ul>
+                        </div>
+                        <div class="col-md-3">
+                            <ul class="nav-multi-list">
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Contemporary Masters</li>
+                                <li class="divider"></li>
+
+                                @foreach ($artists as $artist_each)
+                                @if ($artist_each->genre == "contemporary")
+                                <li><a href="{{ $artist_each->url() }}">{{ $artist_each->inverted_alias() }}</a></li>
+                                @endif
+                                @endforeach
+                            </ul>
+                        </div>
+                        <div class="col-md-3">
+                            <ul class="nav-multi-list">
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Impressionists</li>
+                                <li class="divider"></li>
+
+                                @foreach ($artists as $artist_each)
+                                @if ($artist_each->genre == "impressionist")
+                                <li><a href="{{ $artist_each->url() }}">{{ $artist_each->inverted_alias() }}</a></li>
+                                @endif
+                                @endforeach
+                            </ul>
+                            <ul class="nav-multi-list">
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Old Masters</li>
+                                <li class="divider"></li>
+
+                                @foreach ($artists as $artist_each)
+                                @if ($artist_each->genre == "old")
+                                <li><a href="{{ $artist_each->url() }}">{{ $artist_each->inverted_alias() }}</a></li>
+                                @endif
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
                 </li>
                 <li class="dropdown">
                     <a href="/education" class="dropdown-toggle active" data-toggle="dropdown">Art Education <b class="caret"></b></a>
