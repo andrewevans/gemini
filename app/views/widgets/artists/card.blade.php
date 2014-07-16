e7345c3f33716633c1f006f7f667a2cca5937d3a
andrewe <andrewevansc@gmail.com>
2014-07-16 14:34:23 -0700

    Improved artists cards, especially on artists listing page.
    Former-commit-id: 69523a98dba0a645e8cbe9ef209e4692d3059fbf

--- app/views/widgets/artists/card.blade.php
+++ app/views/widgets/artists/card.blade.php
@@ -1,13 +1,13 @@
 <div class="row">
         @foreach ($artists as $key => $artist)
-        @if ($key % 3 == 0 && $key != 0)
+        @if ($key % 6 == 0 && $key != 0)
             </div>
             <div class="row">
         @endif
-            @if ($key == 0)
-                <div class="col-md-4 col-lg-8 featured">
+            @if ($key < 6)
+                <div class="col-md-2 artists col-lg-4 featured">
             @else
-                <div class="col-md-4">
+                <div class="col-md-2 artists">
             @endif
             <div class="card">
             <figure>
@@ -15,7 +15,7 @@
                     {{ HTML::image($artist->img_url()) }}
                 </a>
                 <figcaption>
-                    <a href="{{ $artist->url() }}"><i>{{ strip_tags($artist->alias) }}</i></a><br />
+                    <a href="{{ $artist->url() }}">{{ $artist->inverted_alias() }}</a><br />
                 </figcaption>
             </figure>
             </div>
