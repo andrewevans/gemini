20edb10574e9f288e3da3c4d0ee7d642f0d0e062
andrewe <andrewevansc@gmail.com>
2014-07-15 22:57:36 -0700

    Improved filters, uses session data to persist through session

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -11,6 +11,12 @@ class BaseController extends Controller {
 	{
         View::share('page_title', SITE_NAME);
 
+        if (isset ($_GET['list'])) {
+            Session::put('list', $_GET['list']);
+        } else {
+            if (! Session::has('list')) Session::put('list', 'card');
+        }
+
 		if ( ! is_null($this->layout))
 		{
 			$this->layout = View::make($this->layout);
