7e611fb8b89cfb9b110dae5ae076d0fb3e24f961
andrewe <andrewevansc@gmail.com>
2014-07-27 15:32:02 -0700

    Netty splashes use data to populate.
    Former-commit-id: 8ee461fae3d9570c220ca30791697ccf625aef76

--- app/views/widgets/splashes_netty.blade.php
+++ app/views/widgets/splashes_netty.blade.php
@@ -1,22 +1,13 @@
+@if (sizeof($splashes) > 0)
 <!-- thumb navigation carousel -->
 <div class="col-md-2 " id="slider-thumbs">
     <!-- thumb navigation carousel items -->
     <ul class="list-unstyled">
-        <li><a id="carousel-selector-0" class="selected carousel-thumb carousel-thumb-3">
-                <h4>Pablo Picasso Ceramics</h4>
-            </a>
-
+        @foreach ($splashes as $key => $splash)
+        <li>
+            <a id="carousel-selector-{{ $key }}" class="selected carousel-thumb carousel-thumb-3"><h4>{{ $splash->title }}</h4></a>
         </li>
-        <li> <a id="carousel-selector-1" class="carousel-thumb carousel-thumb-3">
-                <h4>Marc Chagall, Carmen</h4>
-            </a>
-
-        </li>
-        <li> <a id="carousel-selector-2" class="carousel-thumb carousel-thumb-3">
-                <h4>Beverly Hills Show, August 8th 2014</h4>
-            </a>
-
-        </li>
+        @endforeach
     </ul>
 </div>
 <!-- main slider carousel -->
@@ -25,18 +16,24 @@
             <div id="myCarousel" class="carousel slide">
                 <!-- main slider carousel items -->
                 <div class="carousel-inner">
-                    <div class="active item" data-slide-number="0">
-                        <img src="http://placehold.it/1300x600&amp;text=one" class="img-responsive">
-                    </div>
-                    <div class="item" data-slide-to="1">
-                        <img src="http://placehold.it/600x600/888/FFF" class="img-responsive">
-                    </div>
-                    <div class="item" data-slide-number="2">
-                        <img src="http://placehold.it/600x600&amp;text=three" class="img-responsive">
-                    </div>
+                    @foreach ($splashes as $key => $splash)
+                        <div class="<?= ($key == 0 ? 'active ' : '' ) ?>item" data-slide-to="{{ $key }}">
+                            <div class="col-md-8 item-img">
+                                <a href=""><img src="{{ $splash->asset_url }}" class="img-responsive"></a>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="item-copy">
+                                    <h4>{{ $splash->title }}</h4>
+                                    <p>We like this one because of something or other and it is a piece of art stuff this is. Yes you read that correctly.</p>
+                                    <div class="read-more"><a href="{{ $splash->destination_url }}">Read more &thinsp;&raquo;</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
     </div>
 </div>
 <!--/main slider carousel-->
+@endif
