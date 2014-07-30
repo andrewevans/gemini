3e9883184f1054a0a41c75c7c8d47e8e857bd7bb
andrewe <andrewevansc@gmail.com>
2014-07-30 14:43:22 -0700

    links on artist,post,page cards.

--- app/views/widgets/artists/card_sm.blade.php
+++ app/views/widgets/artists/card_sm.blade.php
@@ -2,7 +2,9 @@
     @foreach ($artists as $key => $artist)
     <div class="col-md-4 artist">
         <div class="artist-img">
-            {{ HTML::image($artist->img_url()) }}
+            <a href="{{ $artist->url() }}">
+                {{ HTML::image($artist->img_url()) }}
+            </a>
         </div>
         <h4><a href="{{ $artist->url() }}">{{ $artist->alias }}</a></h4>
     </div>
--- app/views/widgets/pages.blade.php
+++ app/views/widgets/pages.blade.php
@@ -3,7 +3,9 @@
 @foreach ($pages as $key => $post)
 <div class="col-md-4 post">
     <div class="post-img">
-        <img src="{{ wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'medium' )[0] }}" />
+        <a href="/articles/{{ $post->post_name }}">
+            <img src="{{ wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'medium' )[0] }}" />
+        </a>
     </div>
     <h4><a href="/articles/{{ $post->post_name }}">{{ $post->post_title }}</a></h4>
 </div>
