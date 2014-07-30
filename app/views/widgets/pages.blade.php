03de8c13299785837db2f3ee4bd093ae9b303d55
andrewe <andrewevansc@gmail.com>
2014-07-29 20:32:38 -0700

    Improved netty homepage!

--- app/views/widgets/pages.blade.php
+++ app/views/widgets/pages.blade.php
@@ -1,18 +1,12 @@
-@if (sizeof($pages) > 0)
-    <h2><span class="fa fa-file-text"></span> Featured In-Depth Articles</h2>
+@if (sizeof($posts) > 0)
 
-    @foreach ($pages as $key => $post)
-        <div>
-            @if ($key == 0)
-                <h3 class="featured"><a href="/articles/{{ $post->post_name }}">{{ $post->post_title }}</a></h3>
-            @else
-                <h3><a href="/articles/{{ $post->post_name }}">{{ $post->post_title }}</a></h3>
-            @endif
-        </div>
-    @endforeach
-
-    <div>
-        <a href="/articles">Read more In-Depth Articles</a>
+@foreach ($pages as $key => $post)
+<div class="col-md-4 post">
+    <div class="post-img">
+        <img src="{{ wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'medium' )[0] }}" />
     </div>
+    <h4><a href="/articles/{{ $post->post_name }}">{{ $post->post_title }}</a></h4>
+</div>
+@endforeach
 
 @endif
