0f551718593be0808ffb91c78c26d20c275fb59d
andrewe <andrewevansc@gmail.com>
2014-07-31 13:13:55 -0700

    FIX: IE having trouble clicking filters. Now it can.

--- app/views/widgets/artists/filters.blade.php
+++ app/views/widgets/artists/filters.blade.php
@@ -2,7 +2,7 @@
     @if (sizeof($artist->filters()) > 0)
         <span>Filter By:</span>
         @foreach ($artist->filters() as $filterBy)
-        <button type="button" class="btn btn-default <?= (isset($filter_slug) && $filter_slug == $filterBy ? 'active' : '') ?>"><a href=" {{ $artist->url() . '/' . $filterBy }}">{{ $filterBy }}</a></button>
+        <a href=" {{ $artist->url() . '/' . $filterBy }}"><button type="button" class="btn btn-default <?= (isset($filter_slug) && $filter_slug == $filterBy ? 'active' : '') ?>">{{ $filterBy }}</button></a>
         @endforeach
     @endif
 </div>
