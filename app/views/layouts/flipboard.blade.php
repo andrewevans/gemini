7afe04674014542b32eb9264f21db489246c4563
andrewe <andrewevansc@gmail.com>
2014-09-15 13:13:22 -0700

    links open in app.
    Former-commit-id: 4d8ec2c656b127eb5f2da33ffe0fdd92744550d2

--- app/views/layouts/flipboard.blade.php
+++ app/views/layouts/flipboard.blade.php
@@ -5,6 +5,42 @@
 
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <script>
+        (function(document,navigator,standalone) {
+            // prevents links from apps from oppening in mobile safari
+            // this javascript must be the first script in your <head>
+            if ((standalone in navigator) && navigator[standalone]) {
+                var curnode, location=document.location, stop=/^(a|html)$/i;
+                document.addEventListener('click', function(e) {
+                    curnode=e.target;
+                    while (!(stop).test(curnode.nodeName)) {
+                        curnode=curnode.parentNode;
+                    }
+                    // Condidions to do this only on links to your own app
+                    // if you want all links, use if('href' in curnode) instead.
+                    if('href' in curnode && ( curnode.href.indexOf('http') || ~curnode.href.indexOf(location.host) ) ) {
+                        e.preventDefault();
+                        location.href = curnode.href;
+                    }
+                },false);
+            }
+        })(document,window.navigator,'standalone');
+
+        function logEvent(event) {
+            console.log(event.type);
+            if (event.type == 'cached' || event.type == 'noupdate')
+                alert(event.type);
+        }
+        window.applicationCache.addEventListener('checking', logEvent, false);
+        window.applicationCache.addEventListener('noupdate', logEvent, false);
+        window.applicationCache.addEventListener('downloading', logEvent, false);
+        window.applicationCache.addEventListener('cached', logEvent, false);
+        window.applicationCache.addEventListener('updateready', logEvent, false);
+        window.applicationCache.addEventListener('obsolete', logEvent, false);
+        window.applicationCache.addEventListener('error', logEvent, false);
+    </script>
+
     <title>Experimental Page Layout Inspired by Flipboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="description" content="Experimental Page Layout Inspired by Flipboard" />
