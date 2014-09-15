4c592aa081ee9707adcc62b9990dcfb6715f776e
andrewe <andrewevansc@gmail.com>
2014-09-15 15:20:14 -0700

    FIX: needs slash before ? in URL to be exact.
    Former-commit-id: 835b04318e6a455dfd62e182db19eb82373473a2

--- app/views/artworks/manifest.blade.php
+++ app/views/artworks/manifest.blade.php
@@ -4,6 +4,7 @@ CACHE MANIFEST
 
 CACHE:
 /offline/flipboard?skip=15&page=1
+/offline/flipboard/?skip=15&page=1
 /vendor/flipboard/js/jquery.tmpl.min.js
 /vendor/flipboard/js/jquery.history.js
 /vendor/flipboard/js/core.string.js
