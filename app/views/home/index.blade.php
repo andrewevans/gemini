dc04a8062141cde7635e4add63155b1b1e3817b0
andrewe <andrewevansc@gmail.com>
2015-04-14 21:35:40 -0700

    gemini_lite homepage
    Former-commit-id: 72826983a63a49238155348943727ac027f6c221

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -3,14 +3,14 @@
 @section('title') Login @stop
 
 @section('content')
-
-<div class="container spacy">
+@if (! Config::get('app.gemini_lite'))
+<div class="container spacy splashes-netty">
     <div class="row">
         @include('widgets.splashes_netty')
     </div>
 </div>
 
-<div class="container spacy">
+<div class="container spacy artists-featured">
     <h3>Featured Artists</h3>
     <div class="row">
         @include('widgets.artists.card_sm', array('artists' => $artists_featured))
@@ -19,12 +19,14 @@
         <a href="/artists">Browse All Artists</a>
     </div>
 </div>
+@endif
 
 <div class="container spacy">
     <h3>Our Featured Collection</h3>
     @include('widgets.artworks.card', array('artworks' => $artworks))
 </div>
 
+@if (! Config::get('app.gemini_lite'))
 <div class="container spacy">
     <h3><a href=""><span class="fa fa-rss"></span> Featured Posts</a></h3>
     <div class="row">
@@ -34,12 +36,15 @@
         <a href="http://wp.appelfineart.com">Read more Blog posts</a>
     </div>
 </div>
+@endif
 
+@if (! Config::get('app.gemini_lite'))
 @if (sizeof($artworks_previous) > 0)
     <div class="container">
         <h3>Previously viewed Artworks</h3>
         @include('widgets.artworks.card_sm', array('artworks' => $artworks_previous))
     </div>
+@endif
 @endif
 
 @stop
