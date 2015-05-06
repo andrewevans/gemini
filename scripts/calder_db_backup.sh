de9c2b0d9b6e50eddf28925e69d5515ef6e042da
andrewe <andrewevansc@gmail.com>
2015-05-06 12:46:19 -0700

    removed scripts
    Former-commit-id: 85c35b4f57965370fab8cc2f119e1cee136acd76

--- scripts/calder_db_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "starting calder backup"
-mysqldump --user=calder_mfaport -p -h masterworksfineart.com calder_masterworksnew artists artworks > calder_artists_artworks.sql 
-echo "calder backup complete. starting gemini_calder upload"
-mysql --user=harmony -p -h localhost gemini_calder < calder_artists_artworks.sql
-echo "gemini_calder backup complete"
-
--- scripts/cron_db_transfer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "starting DB transfer..."
-sudo ./calder_db_backup.sh && sudo ./gemini_db_backup.sh && sudo ./gemini_db_import.sh
-
